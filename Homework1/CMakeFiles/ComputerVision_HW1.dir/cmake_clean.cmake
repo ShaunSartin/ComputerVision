94a9e79be1fbdcaac3846a9de91c8e8b20af82e2
ShaunSartin <luckydice2323@yahoo.com>
2018-06-21 14:01:45 -0500

    final version

---
+++ Homework1/CMakeFiles/ComputerVision_HW1.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ComputerVision_HW1.dir/ComputerVision_HW1.cpp.o"
+  "ComputerVision_HW1.pdb"
+  "ComputerVision_HW1"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ComputerVision_HW1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
